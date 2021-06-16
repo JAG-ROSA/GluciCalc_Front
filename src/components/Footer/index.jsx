f3c835443b48677d002c5f45f70075983e0fb864
Morgane Tessier <77018536+m-tessier@users.noreply.github.com>
2021-06-16 15:51:21 +0200

    Revert "Add navbar footer (#12)"
    This reverts commit 892dad0abd0f7e1c63068e2986f01a8f7b2594c8.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,18 +1,8 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Footer = () => (
-  <div className="pt-70">
-    <footer>
-      <div className="d-flex justify-content-between align-items-end">
-        <div className="pe-3 pt-2">
-          <Link to="/" className="title-footer">GluciCalc</Link>
-          <p className="pt-2">© 2021 GluciCalc</p>
-        </div>
-        <p className="text-end">Carole Meney, Arnaud Gossard, Martin Forget et Morgane Tessier</p>
-      </div>
-    </footer>
+  <div>
+    Footer
   </div>
 );
 
