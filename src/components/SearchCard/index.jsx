ead58e3ac568843f0fb2cc2e6dd97366d7774845
martinfzz <forget.l.martin@gmail.com>
2021-06-16 14:02:58 +0200

    Create SearchCardImage component

--- src/components/SearchCard/index.jsx
+++ src/components/SearchCard/index.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-underscore-dangle */
 import React from "react";
 import { Row, Card } from "react-bootstrap";
+import SearchCardImage from "components/SearchCardImage";
 
 const SearchCard = ({ data }) => (
   <div className="d-flex flex-wrap searchCard">
@@ -10,26 +11,22 @@ const SearchCard = ({ data }) => (
         <div key={element._id} className="col-md-6 col-lg-4 py-2">
           <Card style={{ width: "18rem" }}>
             <Card.Body>
-              { (() => {
-                if (element.selected_images !== undefined) {
-                  if (element.selected_images.front !== undefined) {
-                    return (<Card.Img variant="top" src={element.selected_images.front.display.fr} className="productImage" />);
-                  }
-                } else {
-                  return (<Card.Img variant="top" src="../../assets/images/no_product.png" className="productImage" />);
-                }
-              })()}
-
+              <SearchCardImage data={element} />
               <Card.Title>
-                {element.product_name_fr}
-                {element.brands}
+                {element.product_name}
+                &nbsp;
                 -
+                &nbsp;
+                {element.brands}
+                &nbsp;
+                -
+                &nbsp;
                 {element.quantity}
               </Card.Title>
-              {element.nutriscore_grade !== undefined && <Card.Img variant="bottom" src={`https://static.openfoodfacts.org/images/attributes/nutriscore-${element.nutriscore_grade}.svg`} className="nutriscoreImage" />}
-              {element.nova_group !== undefined && <Card.Img variant="bottom" src={`https://static.openfoodfacts.org/images/attributes/nova-group-${element.nova_group}.svg`} className="novagroupImage" />}
-              {element.nutriscore_grade === undefined && <Card.Img variant="bottom" src="https://static.openfoodfacts.org/images/attributes/nutriscore-unknown.svg" className="nutriscoreImage" />}
-              {element.nova_group === undefined && <Card.Img variant="bottom" src="https://static.openfoodfacts.org/images/attributes/nova-group-unknown.svg" className="novagroupImage" />}
+              {element.nutriscore_grade !== undefined && <Card.Img variant="bottom" src={`https://static.openfoodfacts.org/images/attributes/nutriscore-${element.nutriscore_grade}.svg`} className="nutriscoreImage" alt="Representation of the nutriscore value" />}
+              {element.nova_group !== undefined && <Card.Img variant="bottom" src={`https://static.openfoodfacts.org/images/attributes/nova-group-${element.nova_group}.svg`} className="novagroupImage" alt="Representation of the nova group value" />}
+              {element.nutriscore_grade === undefined && <Card.Img variant="bottom" src="https://static.openfoodfacts.org/images/attributes/nutriscore-unknown.svg" className="nutriscoreImage" alt="Representation of the nutriscore value" />}
+              {element.nova_group === undefined && <Card.Img variant="bottom" src="https://static.openfoodfacts.org/images/attributes/nova-group-unknown.svg" className="novagroupImage" alt="Representation of the nova group value" />}
             </Card.Body>
           </Card>
         </div>
