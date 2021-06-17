ef10cb7cabcd0987067807bf205553ad5f1fe4cc
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-17 16:03:03 +0200

    fix importation

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { Form } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
 import { UiManager, UserManager } from "services";
 import Button from "components/Button";
 import { resetErrors } from "store";
