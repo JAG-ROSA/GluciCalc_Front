05125b2c081ac7cfe8e08004a85b12da392e01ce
m-tessier <morgane.tessier22@gmail.com>
2021-06-17 14:39:06 +0200

    update front page Login and Register

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Form } from "react-bootstrap";
 import { UserManager, UiManager } from "services";
+import Button from "components/Button";
 
 const Login = () => {
   const isLoginSuccess = useSelector((store) => store.isLogged);
   const isLoginFailed = useSelector((store) => !!store.error);
+  const history = useHistory();
 
   useEffect(() => {
     if (isLoginSuccess) {
-      UiManager.openNotification("success", "Connexion réussie !");
+      UiManager.openNotification("success", "Connexion réussie ! 😉");
+      history.push("/dashbord");
     } else if (isLoginFailed) {
       UiManager.openNotification(
         "error",
-        "Hum... il y a une petite erreur! 🤔",
+        "Hum... il y a une petite erreur ! 🤔",
       );
     }
   }, [isLoginSuccess, isLoginFailed]);
@@ -29,44 +32,27 @@ const Login = () => {
   };
 
   return (
-    <div className="d-flex justify-content-center pt-5 slide-left">
-      <div className="col col-md-8 col-lg-5 border-shadow border-black p-5 my-5 mx-3">
-        <h2 className="my-text-primary">Se connecter</h2>
+    <div className="d-flex justify-content-center slide-left">
+      <div className="col-sm-10 col-md-8 col-lg-5 border-shadow p-5 mt-5">
+        <h2 className="my-text-primary pb-3">Se connecter</h2>
 
         <Form onSubmit={loginFetch}>
-          <Form.Group controlId="formBasicEmail" className="pb-3">
+          <Form.Group controlId="formBasicEmail" className="fs-6 pb-3">
             <Form.Label>Email</Form.Label>
-            <Form.Control
-              size="sm"
-              type="email"
-              placeholder="nom@example.com"
-            />
+            <Form.Control type="email" placeholder="nom@exemple.com" />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId="formBasicPassword" className="fs-6 pb-3">
             <Form.Label>Mot de passe</Form.Label>
-            <Form.Control
-              size="sm"
-              type="password"
-              placeholder="Mot de passe"
-            />
+            <Form.Control type="password" placeholder="Mot de passe" />
           </Form.Group>
 
-          <button
-            type="submit"
-            className="my-btn-primary my-4"
-          >
-            <span>Se connecter</span>
-          </button>
+          <Button type="submit" content="Se connecter" styles="my-btn-primary my-2" />
         </Form>
 
-        <Link to="/password/forgot" className="link-tertiary">
-          Mot de passe oublié ?
-        </Link>
+        <Link to="/password/forgot" className="link-primary">Mot de passe oublié ?</Link>
         <br />
-        <Link to="/register" className="link-tertiary">
-          S&apos;inscrire
-        </Link>
+        <Link to="/register" className="link-primary">S&apos;inscrire</Link>
       </div>
     </div>
   );
