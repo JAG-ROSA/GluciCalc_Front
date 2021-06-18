079b85dbb7ca1c36fc831af45b51cc5a4c1e4bc3
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-18 12:31:42 +0200

    add destroyMeal function

--- src/services/meals/index.js
+++ src/services/meals/index.js
@@ -5,4 +5,12 @@ export default class MealsManager {
   static getMealsForDay(date) {
     return API.get(`/days/${date}`).then((response) => response.data.map((meal) => new Meal(meal)));
   }
+
+  static async destroyMeal(id) {
+    try {
+      await API.delete(`/meals/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
