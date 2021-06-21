897bd7fe07ae6a54d6a8e902e8b0c414c74efe82
JAG-ROSA <jag.f.rosa@gmail.com>
2021-06-21 12:03:05 +0200

    Create MealItemInput component

---
+++ src/components/MealItemInput/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "components/Button";
+import { Form, Col, Row } from "react-bootstrap";
+
+const MealItem = ({ meal, onDelete, onSave }) => (
+  <div className="detailsMeal">
+    {meal.quantities.map((quantity) => {
+      const food = quantity.product ?? quantity.recipe;
+      return (
+        <div key={quantity.id} className="products d-flex justify-content-between pb-2">
+          <Form onSubmit={(event) => onSave(event, quantity.id)}>
+            <Form.Group as={Row} controlId={`formQuantityInput${quantity.id}`}>
+              <Col sm="10" className="d-flex align-items-baseline">
+                {`${food.name} - `}
+                <Form.Control size="sm" type="text" defaultValue={quantity.quantity} />
+                g
+              </Col>
+            </Form.Group>
+            <div>
+              <Button type="submit" content="Valider" styles="my-btn-tertiary my-btn-sm me-2" />
+              <Button type="button" content="Suprimer" styles="my-btn-tertiary my-btn-sm me-2" onAction={(event) => onDelete(event, quantity.id)} />
+            </div>
+          </Form>
+        </div>
+      );
+    })}
+  </div>
+);
+
+export default MealItem;
