51a11f24d5775d474345ebda388767adf1ff3110
m-tessier <morgane.tessier22@gmail.com>
2021-06-22 12:54:03 +0200

    add component SearchBar to Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import SearchBar from "components/SearchBar";
 
 const Navbar = () => {
   const auth = useSelector((store) => store.isLogged);
+  const location = useLocation();
+  console.log(location.pathname);
 
   return (
     <header>
       <nav>
-        <div className="d-flex justify-content-between align-items-baseline nav">
+        <div className="d-flex justify-content-between align-items-center nav">
           <input type="checkbox" id="nav-check" />
           <h1><Link to="/">GluciCalc</Link></h1>
-          <div className="nav-btn pt-3 pe-4">
-            <label htmlFor="nav-check">
-              <span>{/* // eslint-disable-next-line react/self-closing-comp */}</span>
-              <span>{/* // eslint-disable-next-line react/self-closing-comp */}</span>
-              <span>{/* // eslint-disable-next-line react/self-closing-comp */}</span>
-            </label>
-          </div>
+          {(location.pathname !== "/search")
+            && (
+              <div className="col">
+                <SearchBar />
+              </div>
+            )}
           <ul className="d-flex nav-links">
             {!auth ? (
               <li className="ms-3">
@@ -37,6 +39,13 @@ const Navbar = () => {
               </>
             )}
           </ul>
+          <div className="nav-btn pt-3 pe-4">
+            <label htmlFor="nav-check">
+              <span>{/* // eslint-disable-next-line react/self-closing-comp */}</span>
+              <span>{/* // eslint-disable-next-line react/self-closing-comp */}</span>
+              <span>{/* // eslint-disable-next-line react/self-closing-comp */}</span>
+            </label>
+          </div>
         </div>
       </nav>
     </header>
